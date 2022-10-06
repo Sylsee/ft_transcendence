1d2b5ac52aa0dbba4ed7bceed66031fa88db299a
arnaud35300 <arguilla@student.42.fr>
2022-10-06 19:23:38 +0200

    fix docker config

--- packages/copybackend/src/app.service.ts
+++ packages/copybackend/src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'ft_transcendance :)    ';
+  }
+  getTruc(): string {
+    return 'Truc';
   }
 }
