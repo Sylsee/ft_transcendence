424e8f0659afcfcc432c8387500857bdb4ab30e1
arnaud <arguilla@student.42.fr>
2023-03-06 23:10:28 +0100

    try to use css modules

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./index.css";
 
+import { Header } from "./components/Header/Header";
+
 function App() {
-  const message = "Hello World !";
+  const message = "Hesllo World !";
 
   return (
-    <h1 className="text-4xl font-bold underline text-red-600">{message}</h1>
+    <>
+      <Header />
+      <h1 className="text-4xl font-bold underline text-red-600">{message}</h1>
+    </>
   );
 }
 
---
+++ packages/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import s from "./header.module.css";
+
+export function Header() {
+  return (
+    <>
+      <p className={s.yo}>Yo ;)</p>
+    </>
+  );
+}
