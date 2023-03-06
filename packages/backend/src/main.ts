639f9f1508b90a8b0e45bbd8e2a6412f536216c4
Sylsee <sylvio.poliart@gmail.com>
2023-03-06 02:08:45 +0100

    Delete .env file for security Improve docker_compose.yml and Dockerfile readability Fix gitignore to ignore every .env files Add @nestjs/config package for backend env variable management

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -4,13 +4,16 @@ import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
+  // Create a Nest application instance
   const app = await NestFactory.create(AppModule);
+
+  // Start the application by listening on a port
   await app.listen(3000);
 
+  // Configure hot reloading for development (see https://docs.nestjs.com/techniques/hot-reload)
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
-
 }
 bootstrap();
