fc783958aa0845f6969f6d6467cf7616eb4e012b
arguilla <arguilla@student.42.fr>
2023-03-31 19:12:42 +0200

    feat: 🎸 Add new functions to call the api
    Maybe i need to use the api request use event, but currently i don't
    need it

---
+++ packages/frontend/src/api/api.ts
@@ -0,0 +1,41 @@
+import Cookies from "js-cookie";
+import axios from "axios";
+import { API_BASE_URL, JWT_NAME } from "../config";
+
+const apiClient = axios.create({
+	baseURL: `${API_BASE_URL}`,
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
+
+apiClient.interceptors.request.use((config) => {
+	const cookie = Cookies.get(JWT_NAME);
+	if (cookie) config.headers.Authorization = `Bearer ${cookie}`;
+	return config;
+});
+
+// apiClient.interceptors.response.use(
+// 	(response) => {
+// 		return response;
+// 	},
+// 	(error) => {
+// 		if (error.response) {
+// 			// The request was made and the server responded with a status code
+// 			// that falls out of the range of 2xx
+// 			console.log("Error:", error.response.data);
+// 			console.log(error.response.status);
+// 			console.log(error.response.headers);
+// 		} else if (error.request) {
+// 			// The request was made but no response was received
+// 			console.log(error.request);
+// 		} else {
+// 			// Something happened in setting up the request that triggered an Error
+// 			console.log("Error", error.message);
+// 		}
+// 		console.log(error.config);
+// 		return Promise.reject(error);
+// 	}
+// );
+
+export { apiClient };
---
+++ packages/frontend/src/api/user/userRequests.ts
@@ -0,0 +1,25 @@
+import { API_ROUTES } from "../../config";
+import { UpdateUserRequest } from "../../types/user";
+import { apiClient } from "../api";
+const fetchUserById = async (userId: string) => {
+	const response = await apiClient.get(API_ROUTES.USER(userId));
+	return response.data;
+};
+
+const updateUserById = async (userId: string, data: UpdateUserRequest) => {
+	const formData = new FormData();
+	if (data.name) {
+		formData.append("name", data.name);
+	}
+	if (data.twoFactorAuth) {
+		formData.append("twoFactorAuth", data.twoFactorAuth.toString());
+	}
+	if (data.avatar) {
+		formData.append("avatar", data.avatar);
+	}
+
+	const response = await apiClient.patch(API_ROUTES.USER(userId), formData);
+	return response.data;
+};
+
+export { fetchUserById, updateUserById };
