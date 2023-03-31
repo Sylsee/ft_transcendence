a44e593aca1974bacc1ff94a6b2e39c2af26a53f
arguilla <arguilla@student.42.fr>
2023-03-31 19:16:13 +0200

    feat: 🎸 add the google authentication

--- packages/frontend/src/containers/Home/Home.tsx
+++ packages/frontend/src/containers/Home/Home.tsx
@@ -1,29 +1,43 @@
 import React from "react";
 import { logo42 } from "../../assets/icons/42_logo";
-import { RootState } from "../../store/store-types";
+import { RootState } from "../../types/global";
 import { useSelector } from "react-redux";
-import { BACKEND_URL } from "../../config";
+import { API_BASE_URL, API_ROUTES } from "../../config";
 import { IconButton } from "../../components/IconButton/IconButton";
 import { pong_logo } from "../../assets/icons/pong_logo";
 import { Link } from "react-router-dom";
+import { logoGoogle } from "../../assets/icons/google_logo";
 
 const Home: React.FC = () => {
 	const isAuth = useSelector((store: RootState) => store.AUTH.isAuth);
 
-	const login = async () => {
+	const login = async (url: string) => {
 		if (isAuth) return;
 
-		window.location.href = `${BACKEND_URL}/auth/login`;
+		window.location.href = `${API_BASE_URL}${url}`;
 	};
 
 	return (
 		<div className="h-full flex flex-col   items-center justify-center">
 			{!isAuth && (
-				<IconButton name="Login" onClick={login} logo={logo42()} />
+				<div className="flex">
+					<IconButton
+						url={API_ROUTES.AUTH_42}
+						name="Login"
+						onClick={login}
+						logo={logo42()}
+					/>
+					<IconButton
+						url={API_ROUTES.AUTH_GOOGLE}
+						name="Login"
+						onClick={login}
+						logo={logoGoogle()}
+					/>
+				</div>
 			)}
 			{isAuth && (
 				<Link to="/game">
-					<IconButton name="Play" logo={pong_logo()} />
+					<IconButton url="" name="Play" logo={pong_logo()} />
 				</Link>
 			)}
 		</div>
