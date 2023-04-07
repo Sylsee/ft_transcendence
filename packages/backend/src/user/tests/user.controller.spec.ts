f9f2bf3c233d3049b4d588a97b073f8dd23e7d44
rusmanov <usmanov.riyaz22@gmail.com>
2023-04-07 21:19:03 +0200

    feat: add FriendRequestService at user.controller.spec.ts

--- packages/backend/src/user/tests/user.controller.spec.ts
+++ packages/backend/src/user/tests/user.controller.spec.ts
@@ -2,6 +2,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../user.controller';
 import { UserService } from '../user.service';
+import { FriendRequestService } from '../friend_request.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -9,7 +10,7 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [UserService, FriendRequestService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
