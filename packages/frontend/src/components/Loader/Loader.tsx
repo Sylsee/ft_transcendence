774323a59cf37ad652671de8cf14137e24305721
arguilla <arguilla@student.42.fr>
2023-04-19 19:10:04 +0200

    feat: 🎸 add color property to Loader component

--- packages/frontend/src/components/Loader/Loader.tsx
+++ packages/frontend/src/components/Loader/Loader.tsx
@@ -1,12 +1,16 @@
 import { TailSpin } from "react-loader-spinner";
 
-const Loader: React.FC = () => {
+interface LoaderProps {
+	color?: string;
+}
+
+const Loader: React.FC<LoaderProps> = ({ color = "white" }) => {
 	return (
 		<div className="h-full flex flex-col   items-center justify-center">
 			<TailSpin
 				height="80"
 				width="80"
-				color="white"
+				color={color}
 				ariaLabel="tail-spin-loading"
 				radius="1"
 				wrapperStyle={{}}
