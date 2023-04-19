1101c31d1e95f0b882562137a98a946b68eb8cad
arguilla <arguilla@student.42.fr>
2023-04-19 19:16:36 +0200

    feat: 🎸 improve the components that calls the modal
    WIP

--- packages/frontend/src/components/Profile/ProfileCard/Profile2faAuth/Profile2faAuth.tsx
+++ packages/frontend/src/components/Profile/ProfileCard/Profile2faAuth/Profile2faAuth.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { TwoFactorAuthenticationModal } from "./TwoFactorAuthenticationModal/TwoFactorAuthenticationModal";
 
-interface Profile2faAuthProps {}
+interface Profile2faAuthProps {
+	id: string;
+	twoFactorAuth: boolean;
+}
 
-const Profile2faAuth: React.FC<Profile2faAuthProps> = ({}) => {
+const Profile2faAuth: React.FC<Profile2faAuthProps> = ({
+	id,
+	twoFactorAuth,
+}) => {
+	const [showModal, setShowModal] = useState<boolean>(false);
+
+	const handleToggleChange = () => {
+		if (twoFactorAuth) {
+			// mutation.mutate({ twoFactorAuth: !twoFactorAuth });
+		} else {
+			setShowModal(true);
+		}
+	};
+
 	return (
 		<>
 			<label className="relative inline-flex items-center mr-5 cursor-pointer">
@@ -10,13 +27,18 @@ const Profile2faAuth: React.FC<Profile2faAuthProps> = ({}) => {
 					type="checkbox"
 					value=""
 					className="sr-only peer"
-					onChange={() => console.log("YO")}
+					onChange={handleToggleChange}
+					checked={twoFactorAuth}
 				/>
 				<div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
 				<span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
 					Green
 				</span>
 			</label>
+			<TwoFactorAuthenticationModal
+				showModal={showModal}
+				setShowModal={setShowModal}
+			/>
 		</>
 	);
 };
