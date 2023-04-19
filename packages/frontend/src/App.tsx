f987a64252a1b61c5134315066b97fd53a73b798
arguilla <arguilla@student.42.fr>
2023-04-19 19:37:15 +0200

    feat: 🎸 init the socket connection and disconnection

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,19 +1,21 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ErrorNotFound } from "./components/Error/ErrorNotFound";
 import { Callback } from "./containers/Callback/Callback";
 import { HeaderWrapper } from "./containers/HeaderWrapper/HeaderWrapper";
 import { Home } from "./containers/Home/Home";
 import { Profile } from "./containers/Profile/Profile";
-import { HeaderWrapper } from "./containers/HeaderWrapper/HeaderWrapper";
-import "./index.css";
-import { ErrorPage } from "./pages/ErrorPage/ErrorPage";
 import { ProtectedRoute } from "./containers/ProtectedRoute/ProtectedRoute";
-import { Callback } from "./containers/Callback/Callback";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./index.css";
+import {
+	removeSocketChatListeners,
+	socketChatListeners,
+} from "./sockets/listeners/chatListeners";
+import { connectSocket } from "./sockets/socket";
+import { RootState } from "./types/global";
 
 const router = createBrowserRouter([
 	{
@@ -53,6 +55,17 @@ const router = createBrowserRouter([
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
+	const access_token = useSelector((store: RootState) => store.AUTH.token);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		if (!access_token) return;
+		connectSocket(access_token);
+		socketChatListeners(dispatch);
+		return () => {
+			removeSocketChatListeners();
+		};
+	}, [access_token, dispatch]);
+
 	return (
 		<>
 			<QueryClientProvider client={queryClient}>
