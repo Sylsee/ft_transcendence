30c478d1cc2831fa0f8a2d625c305ebc484d7dd5
Sylsee <sylvio.poliart@gmail.com>
2023-05-02 16:13:35 +0200

    fix: 🐛 Remove null messages in GET /channels/:id/messages route

--- packages/backend/src/chat/services/channel.service.ts
+++ packages/backend/src/chat/services/channel.service.ts
@@ -228,13 +228,16 @@ export class ChannelService {
       throw new InternalServerErrorException();
     }
 
-    return await Promise.all(
-      channel.messages?.map((message) => {
-        if (!userIdInList(user.blockedUsers, message.sender.id)) {
-          return MessageDto.transform(message);
-        }
-      }),
-    );
+    return channel.messages?.reduce(async (accumulatorPromise, message) => {
+      const accumulator = await accumulatorPromise;
+
+      if (!userIdInList(user.blockedUsers, message.sender.id)) {
+        const transformedMessage = MessageDto.transform(message);
+        accumulator.push(transformedMessage);
+      }
+
+      return accumulator;
+    }, Promise.resolve([]));
   }
 
   // ---------------------- User Management ----------------------
