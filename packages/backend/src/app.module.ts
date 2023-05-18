c9ad8ce97b7d3eabe1ab10347951324abd45873f
Sylsee <sylvio.poliart@gmail.com>
2023-05-18 15:29:57 +0200

    refactor: 💡 Separate app.module.ts for single responsibility

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,58 +2,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-// Third-party imports
-import * as Joi from 'joi';
 
 // Local imports
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
 import { ChatModule } from './chat/chat.module';
+import { envValidationConfig } from './config/env-validation.config';
 import { TypeOrmConfigService } from './config/type-orm.config';
 import { GameModule } from './game/game.module';
 import { StaticModule } from './static/static.module';
-import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: `.env.${process.env.NODE_ENV}`,
-      isGlobal: true,
-      expandVariables: true,
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production')
-          .default('development'),
-
-        PORT: Joi.number().default(3000),
-        DB_PORT: Joi.number().default(5432),
-        FRONTEND_PORT: Joi.number().required(),
-        APP_DOMAIN: Joi.string().required(),
-        APP_NAME: Joi.string().required(),
-
-        DB_HOST: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
-        DB_USER: Joi.string().required(),
-        DB_PASS: Joi.string().required(),
-
-        JWT_SECRET: Joi.string().required(),
-        JWT_EXPIRATION_TIME: Joi.number().default(20),
-
-        FORTYTWO_UID: Joi.string().required(),
-        FORTYTWO_SECRET: Joi.string().required(),
-        FORTYTWO_CALLBACK_URL: Joi.string().required(),
-
-        GOOGLE_ID: Joi.string().required(),
-        GOOGLE_SECRET: Joi.string().required(),
-        GOOGLE_CALLBACK_URL: Joi.string().required(),
-      }),
-      validationOptions: {
-        allowUnknown: true,
-        abortEarly: false,
-      },
-    }),
+    ConfigModule.forRoot(envValidationConfig),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useClass: TypeOrmConfigService,
