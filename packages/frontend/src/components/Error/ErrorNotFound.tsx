59961feb9cee0154ee903e06b6e16b3af3515853
arguilla <arguilla@student.42.fr>
2023-05-30 21:22:56 +0200

    fix 404 style

--- packages/frontend/src/components/Error/ErrorNotFound.tsx
+++ packages/frontend/src/components/Error/ErrorNotFound.tsx
@@ -6,7 +6,7 @@ const ErrorNotFound: React.FC<ErrorNotFoundProps> = ({
 	message = "404 not found.",
 }) => {
 	return (
-		<div className="h-full flex justify-center">
+		<div className="h-full flex justify-center w-full">
 			<div className="flex flex-col items-center justify-center  px-4">
 				<p className=" text-7xl text-center font-bold mb-6  ">
 					{message}
